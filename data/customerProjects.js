6ddc3547de8d0479d61e9f700d581ecc359e39b2
divya <kamathd37@gmail.com>
2023-03-14 16:56:36 -0400

    US 18 data

--- data/customerProjects.js
+++ data/customerProjects.js
@@ -1,33 +1,27 @@
 const mongoCollections = require("../config/mongoCollections");
-const projects = mongoCollections.projects;
+const custProjects = mongoCollections.custProjects;
 const { ObjectId } = require("mongodb");
 const validator = require("../helper");
 
-
-const createProject = async (name, location, size, budget, owner,status) => {
-  const project = {
+const customerRequest = async(name, location, size){
+  const customerRequest = {
     name: name,
     location: location,
-    size: size,
-    budget: budget,
-    owner: owner,
-    status: status,
-    date: new Date().toLocaleString()
-  };
-  const projectCollection = await projects();
+    size: size
+  }
+  const projectCollection = await custProjects();
   const insertInfo = await projectCollection.insertOne(project);
   if (!insertInfo.acknowledged || insertInfo.insertedCount === 0)
-    throw "Could not create the project";
-let newId=insertInfo.insertedId;
-const projectinfo = await getProjectById(newId.toString());
-return projectinfo
-};
-
+        throw "Could not create the project";
+    let newId=insertInfo.insertedId;
+    const projectinfo = await getProjectById(newId.toString());
+    return projectinfo
+}
 const getProjectById = async(id)=>{
     if (!validator.validString(id)) throw "id must be given";
     validator.validId(id);
     id = validator.trimString(id);
-    const projectCollection = await projects();
+    const projectCollection = await custProjects();
     const project = await projectCollection.findOne({ _id: new ObjectId(id) });
     if (!project) throw "Project with that id does not exist";
     return project;
@@ -37,16 +31,15 @@ const getProjectsByUserId = async(id)=>{
   if (!validator.validString(id)) throw "id must be given";
     validator.validId(id);
     id = validator.trimString(id);
-    const projectCollection = await projects();
+    const projectCollection = await custProjects();
     const project = await projectCollection.find({ owner: id }).toArray();
     if (!project) throw "Project with that id does not exist";
     return project;
 
 }
 
-
 module.exports = {
-    createProject,
+    customerRequest,
     getProjectById,
     getProjectsByUserId
 }
