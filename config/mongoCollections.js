506cafa7c9465c883488174e44093632d2d92820
Kunal Mandalya <kunalmandalya@Kunals-MacBook-Air.local>
2023-03-22 20:31:54 -0400

    add resource

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -20,5 +20,6 @@ NOTE: YOU WILL NEED TO CHANGE THE CODE BELOW TO HAVE THE COLLECTION(S) REQUIRED 
 module.exports = {
   users: getCollectionFn("users"),
   projects: getCollectionFn("projects"),
-  tasks: getCollectionFn("tasks")
+  tasks: getCollectionFn("tasks"),
+  resources: getCollectionFn("resources")
 };
