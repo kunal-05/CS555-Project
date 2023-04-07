797d21f7b043871314355aea4b70fdb314ffb004
divya <kamathd37@gmail.com>
2023-04-07 11:36:08 -0400

    upload pic

--- data/tasks.js
+++ data/tasks.js
@@ -3,46 +3,57 @@ const tasks = mongoCollections.tasks;
 const { ObjectId } = require("mongodb");
 const validator = require("../helper");
 
-const createTask = async (description, start_date, end_date, task_members, project_id) => {
+const createTask = async (
+  description,
+  start_date,
+  end_date,
+  task_members,
+  project_id,
+  postPicture
+) => {
+  if (!postPicture || postPicture == "") {
+    postPicture = "";
+  }
   const task = {
     description: description,
     start_date: start_date,
     end_date: end_date,
     task_members: task_members,
     project_id: project_id,
+    postPicture: postPicture,
   };
   const taskCollections = await tasks();
   const insertInfo = await taskCollections.insertOne(task);
   if (!insertInfo.acknowledged || insertInfo.insertedCount === 0)
     throw "Could not create the task for the given project";
-const taskInfo = await getTaskById(insertInfo.insertedId.toString());
-return taskInfo
+  const taskInfo = await getTaskById(insertInfo.insertedId.toString());
+  return taskInfo;
 };
 
-const getTaskById = async(id)=>{
-    if (!validator.validString(id)) throw "id must be given";
-    validator.validId(id);
-    id = validator.trimString(id);
-    const taskCollections = await tasks();
-    const task = await taskCollections.findOne({ _id: ObjectId(id) });
-    if (!task) throw "task with that id does not exist";
-    return task;
-}
+const getTaskById = async (id) => {
+  if (!validator.validString(id)) throw "id must be given";
+  validator.validId(id);
+  id = validator.trimString(id);
+  const taskCollections = await tasks();
+  const task = await taskCollections.findOne({ _id: ObjectId(id) });
+  if (!task) throw "task with that id does not exist";
+  return task;
+};
 
-const getTaskByProjectId = async(project_id)=>{
+const getTaskByProjectId = async (project_id) => {
   if (!validator.validString(project_id)) throw "id must be given";
   validator.validId(project_id);
   project_id = validator.trimString(project_id);
   const taskCollections = await tasks();
-  const task = await taskCollections.findOne({ project_id: ObjectId(project_id) });
+  const task = await taskCollections.findOne({
+    project_id: ObjectId(project_id),
+  });
   if (!task) throw "task with that project_id does not exist";
   return task;
-}
-
-
+};
 
 module.exports = {
-    createTask,
-    getTaskById,
-    getTaskByProjectId
-}
+  createTask,
+  getTaskById,
+  getTaskByProjectId,
+};
--- routes/tasks.js
+++ routes/tasks.js
@@ -4,33 +4,86 @@ const mongoCollections = require("../config/mongoCollections");
 const router = express.Router();
 const data = require("../data/");
 //const projects = data.projects;
+const multer = require("multer");
+const path = require("path");
+
 const tasks = data.tasks;
 const validator = require("../helper");
+var fs = require("fs");
 
+// SET STORAGE
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
+var upload = multer({ storage: storage });
 
+router.get("/postpic/:id", async (req, res) => {
+  let errors = [];
+  if (
+    !req.params.id ||
+    typeof req.params.id != "string" ||
+    req.params.id.trim().length == 0 ||
+    !ObjectId.isValid(req.params.id)
+  ) {
+    errors.push("not valid string");
+  }
+  req.params.id = validation.validId(req.params.id);
 
+  const getPost = await posts.getPostById(req.params.id);
+  const profilepicData = getPost.postPicture;
+  if (profilepicData == "") {
+    errors.push("No Post Pic Found!");
+
+    return res.status(400).send({
+      message: "No Post Pic Found!",
+    });
+  } else {
+    res.contentType("image/jpeg");
+    res.send(profilepicData.image.buffer);
+  }
+});
 router.get("/:id", async (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   const taskList = await tasks.getTaskById(id);
   return res.status(200).json(taskList);
 });
 
-router.post("/addTask", async (req, res) => {
-    const body = req.body
-    let description = body.description
-    let start_date= body.start_date
-    let end_date = body.end_date
-    let task_members = body.task_members
-    let project_id = body.project_id
-  const taskList = await tasks.createTask(
-    description,
-    start_date,
-    end_date,
-    task_members,
-    project_id
+router
+  .route("/addTask")
+  .post(upload.single("postPicture"), async (req, res) => {
+    const body = req.body;
+    let description = body.description;
+    let start_date = body.start_date;
+    let end_date = body.end_date;
+    let task_members = body.task_members;
+    let project_id = body.project_id;
+    var finalImg = "";
 
-  );
-  return res.status(200).json(taskList);
-});
+    if (!req.file) {
+      finalImg = "";
+    } else {
+      var img = fs.readFileSync(req.file.path);
+      var encode_image = img.toString("base64");
+      finalImg = {
+        contentType: req.file.mimetype,
+        image: Buffer.from(encode_image, "base64"),
+      };
+    }
+
+    const taskList = await tasks.createTask(
+      description,
+      start_date,
+      end_date,
+      task_members,
+      project_id,
+      finalImg
+    );
+    return res.status(200).json(taskList);
+  });
 
 module.exports = router;
