28944880c51bb13637db2e8f63a0d59af7c19ca5
MANSI MISTRY <65220621+MansiLad@users.noreply.github.com>
2023-04-09 16:33:50 -0400

    user test case works

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,15 +1,19 @@
+const mongoCollections = require("../config/mongoCollections")
 const users = require("../data/users")
+const userColl = mongoCollections.users
 // jest.mock('../request');
 
-test("Crest a user", async() => {
+test("Create a user", async() => {
     const user = await users.createUser(
-        "Kunal",
-        "Mandalya",
-        "kmandalya@gmail.com",
+        "Mansi",
+        "Mistry",
+        "mistry@gmail.com",
         "Test@123",
-        "5512478515",
+        "5513357225",
         "45 Wall Street, NJ 07307",
         "customer"
     )
-    expect(user).toBe("Kunal")
+    const userCollection = await userColl()
+    const search = await userCollection.findOne({firstName: "Mansi"})
+    expect(user).toStrictEqual(search)
 })
